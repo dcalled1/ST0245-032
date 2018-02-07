9240eddab129c59cf9fce8d93e535dc5319c4dbb
asanchezg1 <35736588+asanchezg1@users.noreply.github.com>
2018-02-06 23:19:04 -0500

    Update taller03.java

--- talleres/taller03/taller03.java
+++ talleres/taller03/taller03.java
@@ -10,4 +10,20 @@ public class Taller03
             punto1(n -1, b, a, c);
         }
     }
+    
+    public static void combinations(String base, String s, int j){
+        if (j == 0){
+            for(int i = 0; i < s.length(); i++){
+                combinations("", s.substring(i), 1);
+            }
+        } else if (s.length() == 0){
+            System.out.println(base);
+        }
+        else
+        {
+            for (int i = 1; i <= s.length(); i++){
+                combinations(base + s.charAt(0), s.substring(i), i);
+            }
+        }
+    }
 }
